f40f1bc1ef6d0964ed689e9d0580da852ed08656
freeman <the.freeman.lab@gmail.com>
2016-08-30 14:34:04 -0700

    flesh out api

--- example.py
+++ example.py
@@ -1,4 +1,25 @@
+# alex's version
+
+from tuning import estimate_from_file
+from tuning import estimate_from_array
+
+b, r2 = estimate_from_file('file1.csv', 'file2.csv')
+
+b, r2 = estimate_from_array(X, y)
+
+# pandas version
+
 from tuning import estimate
+
+b, r2 = estimate('file1.csv', 'file2.csv')
+
+# numpy version
+
+from tuning import estimate
+from pandas import read_csv
+
+X = read_csv('file1.csv')
+y = read_csv('file2.csv')
 
 b, r2 = estimate(X, y)
 
--- tuning/main.py
+++ tuning/main.py
@@ -3,4 +3,11 @@ def estimate(X, y):
   Predicting y as a function of X.
 
   Parameters
+  ----------
+  X : ndarray
+    Input variable to regress against.
+  y : ndarray
+    Output variable to predict.
   """
+
+  
