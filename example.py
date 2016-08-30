4c166a2cd4cb1d3d7c295a9523c2bc1d26a3ffd4
freeman <the.freeman.lab@gmail.com>
2016-08-30 14:20:33 -0700

    expand example

--- example.py
+++ example.py
@@ -1,23 +1,14 @@
-from tuning import test
+from tuning import estimate
 
-test()
+b, r2 = estimate(X, y)
 
-from numpy import loadtxt, corrcoef
+# from numpy import loadtxt, corrcoef
 
-X = loadtxt('test/resources/X.csv', delimiter=',')
-y = loadtxt('test/resources/y.csv', delimiter=',')
+# X = loadtxt('test/resources/X.csv', delimiter=',')
+# y = loadtxt('test/resources/y.csv', delimiter=',')
 
 #algorithm = LinearNonlinear(penalty='lasso', alpha=0, delay=0)
 
 #model = algorithm.fit(X, y)
 
 #model.predict(X)
-
-import matplotlib.pyplot as plt
-
-fig = plt.figure(figsize=[15,5])
-plt.plot(X,'r')
-plt.plot(y,'k')
-plt.ylabel('df/f')
-plt.xlabel('time (s)');
-plt.show()
